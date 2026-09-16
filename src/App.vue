f16d59e9501d7b839bcf6f03d25e209c1213deb5
WurgerZax <igoridze.reap@gmail.com>
2026-09-16 12:41:39 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import {computed, ref, watch} from "vue";
+import Button from "@/Button.vue";
+
+const title = ref("World!");
+
+const count = ref(-1);
+
+const setCount = (countChild: number) => {
+  count.value = countChild;
+}
+</script>
+
+<template>
+  <Button :title="title" @clicked="setCount"></Button>
+  <p>Count in child {{count}}</p>
+</template>
+
+<style scoped></style>
---
+++ src/Button.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import {ref} from "vue";
+
+interface Props {
+  title?: string;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  title: 'Hello'
+});
+const emit = defineEmits<{
+  (e: 'clicked', count: number): void
+}>();
+
+const count = ref(0)
+
+const clicked = () => {
+  count.value++;
+  emit('clicked', count.value);
+}
+</script>
+
+<template>
+  <div>
+    <h1>{{props.title}}</h1>
+    <button @click="clicked" title="">Clicked {{count}}</button>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
